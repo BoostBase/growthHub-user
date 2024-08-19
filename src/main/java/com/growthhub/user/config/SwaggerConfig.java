e15c1f6211e7876a9ba0e4d1babd38cd0c20a96c
LDY <leedy3838@naver.com>
2024-08-19 13:32:43 +0900

    rename: 파일 경로 수정

--- src/main/java/com/growthhub/user/config/SwaggerConfig.java
+++ src/main/java/com/growthhub/user/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.growthhub.user.global.config;
+package com.growthhub.user.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
